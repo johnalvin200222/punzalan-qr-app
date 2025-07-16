468cf9f78ed864093659a62b8bc4d97b6a2c4bc2
Elijah-afk-c <christianelijahbaltazar31@gmail.com>
2025-07-16 16:43:04 +0800

    Add project

---
+++ pages/qrscanner.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-container>
+    <v-row justify="center">
+      <v-col cols="12" md="6">
+        <div id="reader" style="width: 100%; max-width: 500px; margin: auto;"></div>
+      </v-col>
+    </v-row>
+  </v-container>    
+</template>
+
+<script>
+import { onMounted } from 'vue'
+import { Html5QrcodeScanner } from 'html5-qrcode'
+
+export default {
+  setup() {
+    // Called when scan is successful
+    const onScanSuccess = (decodedText, decodedResult) => {
+      console.log('Scan successful:', decodedText)
+      // You can do something with the decoded text here
+    }
+
+    // Called on scan failure or error (optional)
+    const onScanFailure = error => {
+      console.warn('Scan error:', error)
+    }
+
+    onMounted(() => {
+      const html5QrcodeScanner = new Html5QrcodeScanner(
+        'reader',
+        { fps: 10, qrbox: { width: 250, height: 250 } },
+        false
+      )
+      html5QrcodeScanner.render(onScanSuccess, onScanFailure)
+    })
+  }
+}
+</script>
